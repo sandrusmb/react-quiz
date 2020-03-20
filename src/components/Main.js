d7b62ad576cfebd79379dfc31e1b188356e6951e
sandrusmb <sandrusmb@gmail.com>
2020-03-20 16:42:18 +0100

    Add id icons sort up and down

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,13 @@ function Main() {
       <h1 className="main__title">Browse Questions</h1>
       <table className="main__table">
         <tr className="main__table__bold">
-          <td className="main__table__bold-item">ID</td>
+          <td className="main__table__bold-item-id">
+            ID
+            <span className="main__table__bold-item-id-span">
+              <i class="fas fa-sort-up"></i>
+              <i class="fas fa-sort-down"></i>
+            </span>
+          </td>
           <td className="main__table__bold-item">Category</td>
           <td className="main__table__bold-item">Type</td>
           <td className="main__table__bold-item">Difficulty</td>
